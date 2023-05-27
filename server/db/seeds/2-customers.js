afc32f724b01c38b0e789239d4550a4b58fe088d
Lauren Lancaster <737lanc@gmail.com>
2023-05-27 17:33:12 +1200

    add log in form to home page

--- server/db/seeds/2-customers.js
+++ server/db/seeds/2-customers.js
@@ -5,5 +5,20 @@ exports.seed = function (knex) {
       first_name: 'Paul',
       last_name: 'Dano',
     },
+    {
+      id: 99902,
+      first_name: 'Selena',
+      last_name: 'Gomez',
+    },
+    {
+      id: 99903,
+      first_name: 'Chris',
+      last_name: 'Hipkins',
+    },
+    {
+      id: 99904,
+      first_name: 'Kim',
+      last_name: 'Possible',
+    },
   ])
 }
