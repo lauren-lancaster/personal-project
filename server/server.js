41c78a8f3b291f63dca4bb3a79332842afd536e5
Lauren Lancaster <737lanc@gmail.com>
2023-06-04 16:44:47 +1200

    add some css

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+const { join } = require('node:path')
+
 const express = require('express')
 const hbs = require('express-handlebars')
 
@@ -7,10 +9,13 @@ const orderRoutes = require('./routes/order')
 const server = express()
 
 // Middleware
+const publicFolder = join(__dirname, 'public')
+server.use(express.static(publicFolder))
+server.use(express.urlencoded({ extended: false }))
+
 server.engine('hbs', hbs.engine({ extname: 'hbs' }))
 server.set('view engine', 'hbs')
 server.set('views', __dirname + '/views')
-server.use(express.urlencoded({ extended: true }))
 
 // Routes
 server.use('/', userRoutes)
