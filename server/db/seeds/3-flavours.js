67efc4c83c0e83e8b59985bf02e7c0c12d7367ba
Lauren Lancaster <737lanc@gmail.com>
2023-06-04 15:30:03 +1200

    add to seeds

--- server/db/seeds/3-flavours.js
+++ server/db/seeds/3-flavours.js
@@ -3,17 +3,42 @@ exports.seed = function (knex) {
     {
       id: 1,
       flavour: 'lychee',
-      drink_name_one: 'fruit',
+      drink_name_one: 'fruity',
     },
     {
       id: 2,
       flavour: 'peach',
-      drink_name_one: 'fruit',
+      drink_name_one: 'pink',
     },
     {
       id: 3,
       flavour: 'yuzu',
-      drink_name_one: 'fruit',
+      drink_name_one: 'tasty',
+    },
+    {
+      id: 4,
+      flavour: 'green',
+      drink_name_one: 'green',
+    },
+    {
+      id: 5,
+      flavour: 'matcha',
+      drink_name_one: 'earthy',
+    },
+    {
+      id: 6,
+      flavour: 'mango',
+      drink_name_one: 'tropical',
+    },
+    {
+      id: 7,
+      flavour: 'taro',
+      drink_name_one: 'sweet',
+    },
+    {
+      id: 8,
+      flavour: 'honeydew',
+      drink_name_one: 'golden',
     },
   ])
 }
--- server/db/seeds/4-toppings.js
+++ server/db/seeds/4-toppings.js
@@ -3,17 +3,27 @@ exports.seed = function (knex) {
     {
       id: 1,
       topping: 'pearls',
-      drink_name_two: 'juice',
+      drink_name_two: 'ocean treasure',
     },
     {
       id: 2,
       topping: 'popping pearls',
-      drink_name_two: 'juice',
+      drink_name_two: 'popping delight',
     },
     {
       id: 3,
       topping: 'jelly',
-      drink_name_two: 'juice',
+      drink_name_two: 'jelly sunset',
+    },
+    {
+      id: 4,
+      topping: 'cookie crumbs',
+      drink_name_two: 'cookie crunch',
+    },
+    {
+      id: 5,
+      topping: 'cheese',
+      drink_name_two: 'mouse trap',
     },
   ])
 }
